919787f10858711ae30429c628c03a3795778877
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2020-12-15 10:34:19 +0700

    Create BankSampah.py

---
+++ BankSampah.py
@@ -0,0 +1,6 @@
+#Imron Assidiqi
+print("Selamat Datang di Layanan Bank Sampah Terpadu")
+print("Pilih Option: ")
+print("1. Login Sebagai Admin")
+print("2. Register Sebagai Nasabah Bank Sampah")
+print("3. Exit")
