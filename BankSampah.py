ee316f139614e26c08d50829b9ae1b0c6e538023
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2020-12-15 12:10:18 +0700

    Update BankSampah.py

--- BankSampah.py
+++ BankSampah.py
@@ -1,13 +1,23 @@
 #Imron Assidiqi
-admin =[]
-nasabah = []
-
-def menu1():
-    print("Selamat Datang di Layanan Bank Sampah Terpadu")
-    print("---Menu---")
-    print("1. Login Sebagai Admin")
-    print("2. Login Sebagai User")
-    print("3. Register Sebagai User Nasabah Bank Sampah")
-    print("4. Exit")
-    print(int(input("Masukkan Menu Yang Anda Pilih :")))
-menu1()
+import getpass
+import os
+print("Selamat Datang di Layanan Bank Sampah Terpadu")
+print("---Menu---")
+print("1. Login Sebagai Admin")
+print("2. Login Sebagai User")
+print("3. Register Sebagai User Nasabah Bank Sampah")
+print("4. Exit")
+menu1=int(input("Masukkan Menu Yang Anda Pilih :"))
+if menu1==1:
+    os.system("clear")
+    hello = ("-Silahkan Masukan Akun admin Anda-")
+    print(hello)
+    user = input ("Username : ")
+    passw = getpass.getpass(prompt="Password : ")
+    password = {"imonassidiqi"}
+    if passw in password:
+        print("selamat " + user + ", Anda berhasil Login")
+    else:
+        print("Username dan Password yang anda masukkan salah")
+if menu1==2:
+    print ("Hallo Bosku")
