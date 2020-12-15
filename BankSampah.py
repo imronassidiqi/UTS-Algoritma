284779f999236d34e78c715f953430159e1579cb
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2020-12-15 11:29:44 +0700

    Update BankSampah.py

--- BankSampah.py
+++ BankSampah.py
@@ -1,6 +1,13 @@
 #Imron Assidiqi
-print("Selamat Datang di Layanan Bank Sampah Terpadu")
-print("Pilih Option: ")
-print("1. Login Sebagai Admin")
-print("2. Register Sebagai Nasabah Bank Sampah")
-print("3. Exit")
+admin =[]
+nasabah = []
+
+def menu1():
+    print("Selamat Datang di Layanan Bank Sampah Terpadu")
+    print("---Menu---")
+    print("1. Login Sebagai Admin")
+    print("2. Login Sebagai User")
+    print("3. Register Sebagai User Nasabah Bank Sampah")
+    print("4. Exit")
+    print(int(input("Masukkan Menu Yang Anda Pilih :")))
+menu1()
