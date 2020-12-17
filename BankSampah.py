1e10c5031d635fa591de91ec6bc1b260a910c1b7
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2020-12-17 19:40:25 +0700

    Update BankSampah.py

--- BankSampah.py
+++ BankSampah.py
@@ -12,57 +12,62 @@ def admin():
     else:
         print("*Username dan Password yang anda masukkan salah")
 def harga():
-    Nasabah = input ("Masukkan Nama Anda : ")
-    hello1 = ("-Silahkan Isi kolom berikut ini-")
+    hello1 = ("--Silahkan Isi kolom berikut ini--")
     print(hello1)
+    Nasabah = input ("Masukkan Nama Lengkap Anda : ")
     user1 = input ("Username : ")
-    berat_sampah = int(input("Masukkan berat sampah anda : "))
+    berat_sampah = int(input("Masukkan berat sampah anda (kg) : "))
     menu = 1
     while menu !=0:
         print ()
-        print(" pilih jenis sampah")
-        print(" 1. Sampah plastik ")
-        print(" 2. Sampah kertas") 
-        print(" 3. Sampah kaleng")
-        print(" 0. exit")
+        print(" ---pilih jenis sampah (harga sampah)---")
+        print(" 1. Sampah plastik (Rp. 2000)")
+        print(" 2. Sampah kertas (Rp. 1500)") 
+        print(" 3. Sampah kaleng (Rp. 3000)")
+        print(" 4. Sampah botol (Rp. 4500)")
+        print(" 0. exit (kembali ke menu utama)")
         menu = int(input("Masukkan jenis sampah : "))
         if menu == 1:
             harga_sampah = berat_sampah * 2000
             print()
-            print("Harga Sampah: ",harga_sampah)
+            print("Harga Sampah anda: Rp.",harga_sampah)
             print()
         elif menu == 2:
             harga_sampah = berat_sampah * 1500
             print()
-            print("Harga sampah: ",harga_sampah)
+            print("Harga sampah anda: Rp.",harga_sampah)
             print()
         elif menu == 3:
             harga_sampah = berat_sampah * 3000
             print()
-            print("Harga sampah: ",harga_sampah)
+            print("Harga sampah anda: Rp.",harga_sampah)
+            print()
+        elif menu == 4:
+            harga_sampah = berat_sampah * 4500
+            print()
+            print("Harga sampah anda: Rp.",harga_sampah)
             print()
         elif menu == 0:
-            print("keluar")
+            print("--keluar---")
         
 pilihan = 1
 while pilihan !=0:
     print()
+    print("=====Selamat Datang di Layanan Bank Sampah======")
     print("---Menu Bank Sampah---")
     print("1. Login Sebagai Admin")
-    print("2. Nasabah ")
+    print("2. Masuk Sebagai Nasabah")
     print("0. exit")
     print()
     pilihan = int(input("Masukkan Nomor Menu: "))
     if pilihan == 1:
         admin()
-        print("Harga Sampah Plastik : 2000")
-        print("Harga Sampah Kertas : 1500")
-        print("Harga Sampah Kaleng : 3000")
     elif pilihan == 2:
-        print("Harga Sampah Plastik : 2000")
-        print("Harga Sampah Kertas : 1500")
-        print("Harga Sampah Kaleng : 3000")
         harga()
         print("--Terimakasih--")
     elif pilihan == 0:
         print("---Terimakasih telah menggunakan Layanan Bank Sampah---")
+        print()
+    else :
+        print("---Pilihan Menu Yang Anda Masukkan Salah---")
+        print()
